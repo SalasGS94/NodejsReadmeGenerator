1d57a9ceb0e047b966f229c5374e43ee6732cb2c
unknown <gergasa94@gmail.com>
2023-06-10 00:12:22 -0600

    license badge

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const MarkDown = require('./utils/generateMarkdown')
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
       type: 'input',
@@ -29,7 +27,7 @@ const questions = [
       type: 'list',
       name: 'license',
       message: 'What kind of license should your project have?',
-      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3'],
+      choices: ['MIT', 'APACHE_2', 'GPL_3', 'BSD_3'],
     },
     {
       type: 'input',
@@ -38,7 +36,7 @@ const questions = [
     },
     {
       type: 'input',
-      name: 'command test',
+      name: 'commandTest',
       message: 'What command should be run to run tests?',
     },
     {
@@ -53,63 +51,6 @@ const questions = [
     },
 ];
 
-
-// inquirer
-//   .prompt([
-//     // {
-//     //   type: 'EXAMPLE1',
-//     //   name: 'name',
-//     //   message: 'What is your GitHub Username?',
-//     // },
-//     // {
-//     //   type: 'EXAMPLE2',
-//     //   name: 'location',
-//     //   message: 'Where are you from?',
-//     // },
-//   ])
-//   .then((answers) => {
-//     const ReadmeContent = generateHTML(answers);
-
-//     fs.writeFile('README.md', ReadmeContent, (err) =>
-//       err ? console.log(err) : console.log('Generating README...')
-//     );
-//   });
-
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-
-// const writeToFile2 = ({ }) =>
-//   `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <header class="p-5 mb-4 header bg-light">
-//     <div class="container">
-//       <h1 class="display-4">Hi! My name is ${name}</h1>
-//       <p class="lead">I am from ${location}.</p>
-//       <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
-//       <ul class="list-group">
-//         <li class="list-group-item">My GitHub username is ${github}</li>
-//         <li class="list-group-item">LinkedIn: ${linkedin}</li>
-//       </ul>
-//     </div>
-//   </header>
-// </body>
-// </html>`;
-
-
-
-
-// TODO: Create a function to initialize app
 function init() {
   return inquirer.prompt(questions)
     .then((answers) => {
@@ -117,15 +58,7 @@ function init() {
       fs.writeFile('README.md', mark, function(err) {
       err ? console.log(err) : console.log('Generating README...')
     })
-    // .catch((error) => {
-    //   console.log(error)
-    // })
-}
+  }
 )}
 
-// What should be written in here????
-
-
-
-// Function call to initialize app
 init();
